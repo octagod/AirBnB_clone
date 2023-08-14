74c397cde7e6963ecfc80be610c5b6de7b2e6020
Semir Muhammed <semirmustafa22@gmail.com>
2023-08-14 03:13:33 +0300

    Task 4 Done

--- models/base_model.py
+++ models/base_model.py
@@ -5,27 +5,40 @@ from datetime import datetime
 
 
 class BaseModel():
-    '''Base Model Class'''
+	'''Base Model Class'''
 
-    def __init__(self):
-        '''init method'''
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+	def __init__(self, *args, **kwargs):
+		'''init method'''
+		if kwargs != {}:
+			for key in kwargs:
+				if key is "__class__":
+					continue
+				elif key is "created_at":
+					self.created_at = datetime.strptime(
+						kwargs[key], '%Y-%m-%dT%H:%M:%S.%f')
+				elif key is "updated_at":
+					self.updated_at = datetime.strptime(
+						kwargs[key], '%Y-%m-%dT%H:%M:%S.%f')
+				else:
+					self.id = kwargs[key]
+		else:
+			self.id = str(uuid.uuid4())
+			self.created_at = datetime.now()
+			self.updated_at = datetime.now()
 
-    def __str__(self):
-        '''string representation'''
-        return f'[{self.__class__.__name__}] ({self.id}) {self.__dict__}'
+	def __str__(self):
+		'''string representation'''
+		return f'[{self.__class__.__name__}] ({self.id}) {self.__dict__}'
 
-    def save(self):
-        '''save method'''
-        self.updated_at = datetime.now()
+	def save(self):
+		'''save method'''
+		self.updated_at = datetime.now()
 
-    def to_dict(self):
-        '''dict method'''
-        diction = {}
-        diction.update(self.__dict__)
-        diction["__class__"] = self.__class__.__name__
-        diction["created_at"] = diction["created_at"].isoformat()
-        diction["updated_at"] = diction["updated_at"].isoformat()
-        return diction
+	def to_dict(self):
+		'''dict method'''
+		diction = {}
+		diction.update(self.__dict__)
+		diction["__class__"] = self.__class__.__name__
+		diction["created_at"] = diction["created_at"].isoformat()
+		diction["updated_at"] = diction["updated_at"].isoformat()
+		return diction
