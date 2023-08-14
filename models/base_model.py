b0f6b9e8188301fd7468c51203f43ee90e643893
Semir Muhammed <semirmustafa22@gmail.com>
2023-08-14 03:49:51 +0300

    Task 4 Done v4

--- models/base_model.py
+++ models/base_model.py
@@ -15,10 +15,10 @@ class BaseModel():
 					continue
 				
 				if key == "created_at":
-					self.created_at = datetime.strptime(
+					self.__dict__["created_at"] = datetime.strptime(
 						kwargs["created_at"], '%Y-%m-%dT%H:%M:%S.%f')
 				elif key == "updated_at":
-					self.updated_at = datetime.strptime(
+					self.__dict__["updated_at"] = datetime.strptime(
 						kwargs["updated_at"], '%Y-%m-%dT%H:%M:%S.%f')
 				else:
 					self.__dict__[key] = kwargs[key]
