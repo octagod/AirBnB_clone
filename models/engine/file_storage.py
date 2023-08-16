d773492072aea11a44d9d8450628b166c57c1757
Semir Muhammed <semirmustafa22@gmail.com>
2023-08-16 05:38:15 +0300

    Task 3 Fix

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,29 +1,39 @@
 #!/usr/bin/python3
-""" File Storage """
+"""
+File Storage
+"""
 import os
 import json
 
 
 class FileStorage():
-	"""Class File Storage used for serialization of instances
-	to a JSON file and deserialization of JSON file to instances"""
+	"""
+	Class File Storage used for serialization of instances
+	to a JSON file and deserialization of JSON file to instances
+	"""
 
 	__file_path = 'file.json'
 	__objects = {}
 
 	def all(self):
-		"""Method used to return the dictionary __objects"""
+		"""
+		Method used to return the dictionary __objects
+		"""
 
 		return FileStorage.__objects
 	
 	def new(self, obj):
-		"""Method used to set __objects with obj's class name
-		and id in the form of <obj class_name>.<obj.id>"""
+		"""
+		Method used to set __objects with obj's class name
+		and id in the form of <obj class_name>.<obj.id>
+		"""
 
 		FileStorage.__objects[f"{obj.__class__.__name__}.{obj.id}"] = obj
 
 	def save(self):
-		"""Method used to serialize __objects to the JSON file"""
+		"""
+		Method used to serialize __objects to the JSON file
+		"""
 
 		__objects_copy = {}
 		for key, value in FileStorage.__objects.items():
@@ -32,7 +42,9 @@ class FileStorage():
 			json.dump(__objects_copy, json_file)
 	
 	def reload(self):
-		"""Method used to deserialize JSON file in to __objects"""
+		"""
+		Method used to deserialize JSON file in to __objects
+		"""
 		from models.base_model import BaseModel
 		from models.user import User
 		from models.state import State
@@ -52,6 +64,6 @@ class FileStorage():
 				}
 		exists = os.path.isfile(FileStorage.__file_path)
 		if exists:
-			with open(FileStorage.__file_path, 'r', encoding='utf-8') as f:
-				for key, val in json.load(f).items():
-					FileStorage.__objects[key] = classes[val['__class__']](**val)
+			with open(FileStorage.__file_path, 'r', encoding='utf-8') as file:
+				for key, value in json.load(file).items():
+					FileStorage.__objects[key] = classes[value['__class__']](**value)
